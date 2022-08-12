5be6aa32e9f05390420445976f18b09327415dfa
phillip-vo <philvo1991@gmail.com>
2022-08-12 13:58:22 -0500

    Add card and footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { Container } from "./components/styles/Container.styled";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Card from "./components/Card";
 import GlobalStyles from "./components/styles/Global";
 import { ThemeProvider } from "styled-components";
+import content from "./content";
 
 const theme = {
   colors: {
@@ -9,6 +12,7 @@ const theme = {
     body: "#fff",
     footer: "#003333",
   },
+  mobile: "768px",
 };
 
 function App() {
@@ -18,8 +22,11 @@ function App() {
         <GlobalStyles />
         <Header />
         <Container>
-          <h1>Hello World</h1>
+          {content.map((item, index) => (
+            <Card key={index} item={item} />
+          ))}
         </Container>
+        <Footer />
       </>
     </ThemeProvider>
   );
